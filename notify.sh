7f6c4178c6f8fd3672f297535a81c825968b766a
Florian Olive <florian.olive@gmail.com>
2026-08-04 12:41:26 +0200

    chore(ops): fix notify.sh shebang and widen the alert filter
    Adds a shebang, makes the pattern case-insensitive and swaps the unused
    'deploy' term for KMNERF so anticheat nerfs come through.

--- notify.sh
+++ notify.sh
@@ -1,1 +1,3 @@
- tail -F /home/feho/.openmohaa/main/qconsole.log | rg 'aimbot|says|deploy' --line-buffered | rg -v 127.0.0.1 --line-buffered | xargs -r -d '\n' -I{} curl -fsS --data-binary "{}" https://ntfy.sh/my-mohaa-monitoring-example
+#!/bin/bash
+
+tail -F /home/feho/.openmohaa/main/qconsole.log | rg -i 'aimbot|says|KMNERF' --line-buffered | rg -v 127.0.0.1 --line-buffered | xargs -r -d '\n' -I{} curl -fsS --data-binary "{}" https://ntfy.sh/my-mohaa-monitoring-example
