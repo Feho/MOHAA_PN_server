ebcae6934b69cbc75477c6afd65d04ef09e51759
Florian Olive <florian.olive@gmail.com>
2026-08-24 18:13:06 +0200

    chore: update notify.sh

--- notify.sh
+++ notify.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tail -F /home/feho/.openmohaa/main/qconsole.log | rg -i 'aimbot|says|KMNERF' --line-buffered | rg -v 127.0.0.1 --line-buffered | xargs -r -d '\n' -I{} curl -fsS --data-binary "{}" https://ntfy.sh/my-mohaa-monitoring-example
+tail -F /home/feho/.openmohaa/main/qconsole.log | rg -i -o '.+aimbot.+|} .+ says .+|.+KMNERF.+' --line-buffered | rg -v '127.0.0.1|filtered out' --line-buffered | xargs -r -d '\n' -I{} curl -fsS --data-binary "{}" https://ntfy.sh/my-mohaa-monitoring-example
